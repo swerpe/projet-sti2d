f82aa0f7016a7a0ea7331ce7ed2ce1d163e59a0b
swerpe <galagougou@proton.me>
2025-02-27 00:08:13 +0100

    Update main.ino
    sachant que jsuis un gros prouveur de merde et que j'apprends les pointeurs actuellement jme suis dis on va remplacer de code de con avec des pointeurs ptn jsuis trop smart mais qui va lire ça par contre

--- main.ino
+++ main.ino
@@ -22,18 +22,15 @@
 #define secondSpeed 160
 #define thirdSpeed 250
 
-// Variables pour le maintien de la pression
-unsigned long startedTime = 0;
-int currentSpeed = 0;
-int lastState = 0; // 0 = null / 1 = up / 2 = down
-int timeScdSpeed = 3000; // 3s avant de passer à la 2e vitesse
-int timeThdSpeed = 3000; // 3s avant de passer à la 3e vitesse
+// 3s avant de passer à la 2e vitesse
+#define timeScdSpeed 3000
 
-// Variable pour les petites pressions
-unsigned long lastPressionUP = 0;
-unsigned long lastPressionDOWN = 0;
+// 3s avant de passer à la 3e vitesse
+#define timeThdSpeed 3000
 
-void setup(){
+int main(){
+  
+  init(); // on initialise les trucs d'arduino pour que pinmode tt ça fonctionne
   
   // pinMode du moteur:
   pinMode(motordriver, OUTPUT);
@@ -45,49 +42,56 @@ void setup(){
   // pinMode des boutons:
   pinMode(up, INPUT);
   pinMode(down, INPUT);
+  
+  // Variables pour le maintien de la pression
+  int lastState = 0; // 0 = null / 1 = up / 2 = down
+  unsigned long startedTime = 0;
+  int currentSpeed = 0;
+  
+  Serial.begin(9600);
+  Serial.println("salut");
+  while (true){
+    Serial.println("true");
+    // On récupère l'état des boutons et on regarde si appuyé:
+    if(digitalRead(up) && digitalRead(down)){ // Si les 2 boutons sont appuyés
 
-}
+      // On coupe le moteur
+      motorSpeed(0,0);
+      changeState(0, &lastState, &startedTime, &currentSpeed);
 
-void loop(){
-  // On récupère l'état des boutons et on regarde si appuyé:
-  if(digitalRead(up) && digitalRead(down)){ // Si les 2 boutons sont appuyés
-    
-    // On coupe le moteur
-    motorSpeed(0,0);
-    changeState(0);
-    
-  } else if (digitalRead(up)){ // Si seulement le bouton pour monter est appuyé
-    
-    if(lastState != 1) changeState(1);
-    
-    // On gère le maintien
-    maintien();
-    
-    // On envoie le courant à la vitesse adéquate
-	motorSpeed(currentSpeed, 0);
-    
-  } else if (digitalRead(down) == 1) { // Si seulement le bouton descendre est appuyé
-    
-    if(lastState != 2) changeState(2);
-    
-    // On gère le maintien
-    maintien();
-    
-    // On envoie le courant à la vitesse adéquate en "inversant" le courant
-	motorSpeed(0, currentSpeed);
-    
-  } else { // Si on appuie pas, on coupe le moteur ET on dit qu'on appuie pas
-    motorSpeed(0, 0);
-    changeState(0);
-  } 
-  delay(10);
+    } else if (digitalRead(up)){ // Si seulement le bouton pour monter est appuyé
+	  
+      if(lastState != 1) changeState(1, &lastState, &startedTime, &currentSpeed);
+
+      // On gère le maintien
+      maintien(&currentSpeed, startedTime);
+
+      // On envoie le courant à la vitesse adéquate
+      motorSpeed(currentSpeed, 0);
+
+    } else if (digitalRead(down) == 1) { // Si seulement le bouton descendre est appuyé
+
+      if(lastState != 2) changeState(2, &lastState, &startedTime, &currentSpeed);
+
+      // On gère le maintien
+      maintien(&currentSpeed, startedTime);
+
+      // On envoie le courant à la vitesse adéquate en "inversant" le courant
+      motorSpeed(0, currentSpeed);
+
+    } else { // Si on appuie pas, on coupe le moteur ET on dit qu'on appuie pas
+      motorSpeed(0, 0);
+      changeState(0, &lastState, &startedTime, &currentSpeed);
+    } 
+    delay(10);
+  }
 }  
 
-void maintien(){
-  if(millis() - startedTime >= timeScdSpeed && currentSpeed == firstSpeed){
-       currentSpeed = secondSpeed; // on dit qu'on est à la 2nde vitesse
-    } else if(millis() - startedTime >= (timeThdSpeed+timeScdSpeed) && currentSpeed == secondSpeed){
-       currentSpeed = thirdSpeed; // on dit qu'on est à la 3e vitesse
+void maintien(int* currentSpeed, int startedTime){
+  if(millis() - startedTime >= timeScdSpeed && *currentSpeed == firstSpeed){
+       *currentSpeed = secondSpeed; // on dit qu'on est à la 2nde vitesse
+    } else if(millis() - startedTime >= (timeThdSpeed+timeScdSpeed) && *currentSpeed == secondSpeed){
+       *currentSpeed = thirdSpeed; // on dit qu'on est à la 3e vitesse
     }
 }
 
@@ -96,8 +100,8 @@ void motorSpeed(int plus, int moins){
   analogWrite(negative, moins);
 }  
 
-void changeState(int state){
-  lastState = state;
-  startedTime = millis();
-  currentSpeed = firstSpeed;
+void changeState(int state, int* lastState, unsigned long* startedTime, int* currentSpeed){
+  *lastState = state;
+  *startedTime = millis();
+  *currentSpeed = firstSpeed;
 }  
